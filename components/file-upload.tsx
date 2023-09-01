4ba02e7a23bde2600c844f7589c80a559466c1ba
Sakil Khan <sakilk130@gmail.com>
2023-09-01 22:56:34 +0600

    feat: create initial modal

---
+++ components/file-upload.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import React, { FC } from 'react';
+import { X } from 'lucide-react';
+import Image from 'next/image';
+
+import { UploadDropzone } from '@/lib/uploadthing';
+
+import '@uploadthing/react/styles.css';
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: 'messageFile' | 'serverImage';
+}
+
+const FileUpload: FC<FileUploadProps> = ({ endpoint, onChange, value }) => {
+  const fileType = value?.split('.').pop();
+
+  if (value && fileType !== 'pdf') {
+    return (
+      <div className="relative w-20 h-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange('')}
+          className="absolute top-0 right-0 p-1 text-white rounded-full shadow-sm bg-rose-500"
+          type="button"
+        >
+          <X className="w-4 h-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
+
+export { FileUpload };
