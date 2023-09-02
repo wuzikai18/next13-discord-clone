bf80d8f1f0c47b08bf673eff04c910e3b1f31e92
Sakil Khan <sakilk130@gmail.com>
2023-09-02 11:02:28 +0600

    feat: create invite modal and api

---
+++ hooks/use-origin.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+const useOrigin = () => {
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const origin =
+    typeof window !== 'undefined' && window.location.origin
+      ? window.location.origin
+      : '';
+
+  if (!mounted) {
+    return '';
+  }
+
+  return origin;
+};
+
+export { useOrigin };
